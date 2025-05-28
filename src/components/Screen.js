afd701445babe3d7b81dc1d05796f96278a7c3cd
ShashankBairy <shashankshivakarthik007@gamil.com>
2025-05-29 00:36:56 +0530

    third

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,8 +1,187 @@
-
+import "./Screen.css"
+import profilelogo from "../assets/profileimage.jpg"
 const Screen = () =>{
     return(
-        <div>
-            <h1>Screen</h1>
+        <div className="student_container">
+           <section className="student_content">
+               <div className="routing_container">
+                  <div className="route_content">
+                      <p>Student Data</p>
+                  </div>
+                  <div className="route_content ">
+                      <p>Fee Payments</p>
+                  </div>
+                  <div className="route_content">
+                     <p>Fee Adjustments</p>
+                  </div>
+               </div>
+               <div className="student_details">
+                <div className="student_details_1">
+                    <div className="student_details_content">
+   <div className="student_details_content_1">
+                     <h1>Student Details</h1>
+                     <p>Comprehensive Student Profile Including Academics, Career Progress, Olympiads, And Extracurricular Activities.</p>
+                  </div>
+                  <div className="student_details_history_button">
+                    <button>
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 15.75C7.425 15.75 6.03125 15.272 4.81875 14.316C3.60625 13.36 2.81875 12.138 2.45625 10.65C2.40625 10.4625 2.44375 10.2907 2.56875 10.1347C2.69375 9.97875 2.8625 9.888 3.075 9.8625C3.275 9.8375 3.45625 9.875 3.61875 9.975C3.78125 10.075 3.89375 10.225 3.95625 10.425C4.25625 11.55 4.875 12.4688 5.8125 13.1813C6.75 13.8938 7.8125 14.25 9 14.25C10.4625 14.25 11.7032 13.7407 12.7222 12.7222C13.7412 11.7037 14.2505 10.463 14.25 9C14.2495 7.537 13.7402 6.2965 12.7222 5.2785C11.7042 4.2605 10.4635 3.751 9 3.75C8.1375 3.75 7.33125 3.95 6.58125 4.35C5.83125 4.75 5.2 5.3 4.6875 6H6C6.2125 6 6.39075 6.072 6.53475 6.216C6.67875 6.36 6.7505 6.538 6.75 6.75C6.7495 6.962 6.6775 7.14025 6.534 7.28475C6.3905 7.42925 6.2125 7.501 6 7.5H3C2.7875 7.5 2.6095 7.428 2.466 7.284C2.3225 7.14 2.2505 6.962 2.25 6.75V3.75C2.25 3.5375 2.322 3.3595 2.466 3.216C2.61 3.0725 2.788 3.0005 3 3C3.212 2.9995 3.39025 3.0715 3.53475 3.216C3.67925 3.3605 3.751 3.5385 3.75 3.75V4.7625C4.3875 3.9625 5.16575 3.34375 6.08475 2.90625C7.00375 2.46875 7.9755 2.25 9 2.25C9.9375 2.25 10.8158 2.42825 11.6347 2.78475C12.4537 3.14125 13.1663 3.62225 13.7723 4.22775C14.3783 4.83325 14.8595 5.54575 15.216 6.36525C15.5725 7.18475 15.7505 8.063 15.75 9C15.7495 9.937 15.5715 10.8152 15.216 11.6347C14.8605 12.4542 14.3793 13.1668 13.7723 13.7723C13.1653 14.3778 12.4527 14.859 11.6347 15.216C10.8167 15.573 9.9385 15.751 9 15.75ZM9.75 8.7L11.625 10.575C11.7625 10.7125 11.8312 10.8875 11.8312 11.1C11.8312 11.3125 11.7625 11.4875 11.625 11.625C11.4875 11.7625 11.3125 11.8312 11.1 11.8312C10.8875 11.8312 10.7125 11.7625 10.575 11.625L8.475 9.525C8.4 9.45 8.34375 9.36575 8.30625 9.27225C8.26875 9.17875 8.25 9.08175 8.25 8.98125V6C8.25 5.7875 8.322 5.6095 8.466 5.466C8.61 5.3225 8.788 5.2505 9 5.25C9.212 5.2495 9.39025 5.3215 9.53475 5.466C9.67925 5.6105 9.751 5.7885 9.75 6V8.7Z" fill="black"/>
+</svg>
+                        History
+                    </button>
+                       
+                  </div>
+                    </div>
+                    <div className="student_routing_container">
+                       <div className="student_route">
+                          <p>Basic Details</p>
+                       </div>
+                       <div className="student_route">
+                           <p>Feed Back</p>
+                       </div>
+                       <div className="student_route">
+                            <p>Marks</p>
+                       </div>
+                       <div className="student_route">
+                            <p>Career Progress</p>
+                       </div>
+                       <div className="student_route">
+                             <p>Olympiads</p>
+                       </div>
+                       <div className="student_route">
+                            <p>Extra Curricular Activites</p>
+                       </div>
+                    </div>
+                </div>
+                 <div className="student_details_2">
+                    <div className="student_details_grid">
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                       <div className="student_detail_grid">
+                          <p className="student_detail_heading">Parent Name</p>
+                          <p className="student_detail_value">Giridhar Bhimineni</p>
+                       </div>
+                    </div>
+                    <div className="student_details_non_grid">
+                        <p className="student_detail_heading">Section</p>
+                        <p className="student_detail_value">JR_NEON_NPL_BATCH_3-JNPL3</p>
+                    </div>
+                     <div className="student_details_non_grid">
+                        <p className="student_detail_heading">Address</p>
+                        <p className="student_detail_value">JR_NEON_NPL_BATCH_3-JNPL3</p>
+                    </div>
+                 </div>
+               </div>
+           </section>
+           <section className="student_profile">
+               <div className="student_profile_content_1">
+                   <div className="student_profile_heading">
+                       <h1>Profile</h1>
+                   </div>
+                   <div className="student_profile_image">
+                        <figure>
+                           <img className="profile_image" src={profilelogo} alt="profile-image"/>
+                        </figure>
+                   </div>
+                   <div className="student_profile_name">
+                        <h1>Yaswanth Bhimineni</h1>
+                   </div>
+                   <div className="student_profile_gender">
+                        <p>Male</p>
+                   </div>
+                   <div className="student_profile_standard">
+                       <div className="student_branch">
+                            <p>MPC</p>
+                        </div>
+                        <div className="student_year">
+                             <p>Sr. Inter</p>
+                        </div>
+                   </div>
+                   <div className="student_information_icons ">
+                         <div className="student_message_icon">
+<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.6666 4.33325H4.33329C3.13668 4.33325 2.16663 5.3033 2.16663 6.49992V19.4999C2.16663 20.6965 3.13668 21.6666 4.33329 21.6666H21.6666C22.8632 21.6666 23.8333 20.6965 23.8333 19.4999V6.49992C23.8333 5.3033 22.8632 4.33325 21.6666 4.33325Z" stroke="#616161" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M23.8333 7.58325L14.1158 13.7583C13.7813 13.9678 13.3946 14.0789 13 14.0789C12.6053 14.0789 12.2186 13.9678 11.8841 13.7583L2.16663 7.58325" stroke="#616161" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                           </div>
+                           <div className="student_contact_icon">
+<svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.05658 2.41795L6.22358 2.06619C7.54608 1.66755 8.95908 2.31179 9.52544 3.57161L10.4271 5.57733C10.9098 6.65117 10.6554 7.91317 9.79439 8.71612L8.30009 10.1096C8.25642 10.1503 8.22876 10.2053 8.22213 10.2647C8.17778 10.6618 8.44701 11.4352 9.06727 12.5095C9.51832 13.2908 9.92657 13.8387 10.2741 14.1468C10.5162 14.3615 10.6494 14.4079 10.7062 14.391L12.7165 13.7764C13.8421 13.4323 15.0619 13.8426 15.7506 14.7971L17.0313 16.5721C17.8374 17.6893 17.6925 19.2306 16.6922 20.1779L15.8058 21.0173C14.8494 21.9231 13.4879 22.2606 12.2191 21.9065C9.46503 21.1378 6.99572 18.8137 4.78384 14.9826C2.56889 11.1462 1.7917 7.84173 2.50815 5.06994C2.83602 3.80146 3.80215 2.79607 5.05658 2.41795ZM5.48948 3.85413C4.73682 4.081 4.15715 4.68423 3.96043 5.44532C3.35778 7.77684 4.04663 10.7057 6.08288 14.2326C8.11641 17.7548 10.3054 19.815 12.6224 20.4617C13.3836 20.6742 14.2006 20.4717 14.7744 19.9282L15.6608 19.0888C16.1154 18.6582 16.1813 17.9576 15.8149 17.4498L14.5342 15.6748C14.2211 15.241 13.6667 15.0544 13.155 15.2109L11.1397 15.827C9.97 16.1757 8.90832 15.2342 7.76823 13.2595C6.99993 11.9288 6.64183 10.9001 6.7314 10.0982C6.7778 9.68273 6.97137 9.29763 7.27709 9.01254L8.77139 7.61909C9.16278 7.25411 9.27842 6.68047 9.05899 6.19237L8.15732 4.18665C7.89989 3.614 7.25762 3.32116 6.65648 3.50236L5.48948 3.85413Z" fill="#616161"/>
+</svg>
+                           </div>  
+                    </div>
+               </div>
+               <hr className="student_profile_line"></hr>
+               <div className="student_profile_content_2">
+                  <div className="student_profile_recent_academics">
+                       <div className="student_recent_academics_heading">
+                          <h1>Recent Academics</h1>
+                       </div>
+                       <div className="student_recent_marks">
+                          <p>Weekly Test</p>
+                          <p>411</p>
+                       </div>
+                       <div className="student_recent_marks">
+                          <p>Weekly Test</p>
+                          <p>411</p>
+                       </div>
+                       <div className="student_recent_marks">
+                          <p>Weekly Test</p>
+                          <p>411</p>
+                       </div>
+                       <div className="student_recent_marks">
+                          <p>Weekly Test</p>
+                          <p>411</p>
+                       </div>
+                       <div className="student_recent_marks">
+                          <p>Weekly Test</p>
+                          <p>411</p>
+                       </div>
+                  </div>
+               </div>
+           </section>
         </div>
     )
 }
